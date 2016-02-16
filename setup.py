2fd77758acc7674316ad79b41159d0424981d828
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2016-02-16 11:13:43 +0100

    cleaning

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ def readme():
         return f.read()
 
 setup(
-    name='wishful_wifi_handover',
+    name='wishful_module_handover',
     version='0.1.0',
     packages=find_packages(),
     url='http://www.wishful-project.eu/software',
     license='',
     author='Piotr Gawlowicz',
     author_email='gawlowicz@tu-berlin.de',
-    description='WiSHFUL WIFI Handover Module',
+    description='WiSHFUL Handover Module',
     long_description='Implementation of a wireless agent using the unified programming interfaces (UPIs) of the Wishful project.',
     keywords='wireless control',
     install_requires=[]
--- wishful_module_handover/handover_module.py
+++ wishful_module_handover/handover_module.py
@@ -1,6 +1,6 @@
 import logging
+import wishful_controller
 import wishful_upis as upis
-
 
 __author__ = "Piotr Gawlowicz, Anatolij Zubow"
 __copyright__ = "Copyright (c) 2015, Technische Universitat Berlin"
@@ -8,14 +8,14 @@ __version__ = "0.1.0"
 __email__ = "{gawlowicz, zubow}@tkn.tu-berlin.de"
 
 
-@wishful_module.build_module
-class WiFiHandover(wishful_module.WishfulModule):
-    def __init__(self, agentPort=None):
-        super(WiFiHandover, self).__init__(agentPort)
+@wishful_controller.build_module
+class HandoverModule(wishful_controller.ControllerUpiModule):
+    def __init__(self, controller):
+        super(HandoverModule, self).__init__(controller)
         self.log = logging.getLogger('wifi_handover_module.main')
 
 
-    @wishful_module.bind_function(upis.global_upi.perform_hard_handover)
+    @wishful_controller.bind_function(upis.global_upi.perform_hard_handover)
     def perform_hard_handover(self, srcAp, dstAp, station):
         self.log.debug("Hard handover of station: {} from {} to {}".format(station, srcAp, dstAp))
         #call set of function here, like
@@ -27,7 +27,7 @@ class WiFiHandover(wishful_module.WishfulModule):
         return retVal
 
 
-    @wishful_module.bind_function(upis.global_upi.perform_soft_handover)
+    @wishful_controller.bind_function(upis.global_upi.perform_soft_handover)
     def perform_soft_handover(self, srcAp, dstAp, station):
         self.log.debug("Soft handover of station: {} from {} to {}".format(station, srcAp, dstAp))
         #call set of function here, like
