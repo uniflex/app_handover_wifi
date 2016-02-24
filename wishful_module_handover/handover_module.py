65277153d0e1267e194093d9c07ddbb66b5e41e8
zubow <zubow@tkn.tu-berlin.de>
2016-02-24 23:40:10 +0100

    finding better function names in radio/net/global for unification.

--- wishful_module_handover/handover_module.py
+++ wishful_module_handover/handover_module.py
@@ -17,50 +17,36 @@ class HandoverModule(wishful_controller.ControllerUpiModule):
         super(HandoverModule, self).__init__(controller)
         self.log = logging.getLogger('wifi_handover_module.main')
 
+    @wishful_controller.bind_function(upis.global_upi.perform_handover)
+    def perform_handover(self, wifi_intf, servingAP, targetAP, sta_mac_addr, **kwargs):
 
-    @wishful_controller.bind_function(upis.global_upi.perform_hard_handover)
-    def perform_hard_handover(self, wifi_intf, wlan_inject_iface, sta_mac_addr, sta_ip_addr,
-                             servingAP, servingAP_ip_addr, servingAP_channel, servingAP_bssid,
-                             targetAP, targetAP_ip_addr, targetAP_channel, gateway):
-
-        self.log.debug("Hard handover of station: {} from {} to {}".format(sta_mac_addr, servingAP_ip_addr, targetAP_ip_addr))
-
-        return self.perform_STA_handover(wifi_intf, wlan_inject_iface, sta_mac_addr, sta_ip_addr,
-                                         servingAP, servingAP_ip_addr, servingAP_channel, servingAP_bssid,
-                                         targetAP, targetAP_ip_addr, targetAP_channel, gateway, HandoverTypes.Hard_BL)
-
-    @wishful_controller.bind_function(upis.global_upi.perform_soft_handover)
-    def perform_soft_handover(self, wifi_intf, wlan_inject_iface, sta_mac_addr, sta_ip_addr,
-                             servingAP, servingAP_ip_addr, servingAP_channel, servingAP_bssid,
-                             targetAP, targetAP_ip_addr, targetAP_channel, gateway):
-
-        self.log.debug("Soft handover of station: {} from {} to {}".format(sta_mac_addr, servingAP_ip_addr, targetAP_ip_addr))
-
-        return self.perform_STA_handover(wifi_intf, wlan_inject_iface, sta_mac_addr, sta_ip_addr,
-                                         servingAP, servingAP_ip_addr, servingAP_channel, servingAP_bssid,
-                                         targetAP, targetAP_ip_addr, targetAP_channel, gateway, HandoverTypes.Soft_CSA)
-
-
-    def perform_STA_handover(self, wifi_intf, wlan_inject_iface, sta_mac_addr, sta_ip_addr,
-                             servingAP, servingAP_ip_addr, servingAP_channel, servingAP_bssid,
-                             targetAP, targetAP_ip_addr, targetAP_channel, gateway, hoType=HandoverTypes.Hard_BL):
         """
         Performing an infrastructure-initiated handover of a client STA using either:
-        - hard HO scheme,
-        - soft HO scheme
+        - ho_type = 0: hard HO scheme,
+        - ho_type = 1: soft HO scheme
         """
 
-        self.log.info('Function: performSTAHandover')
+        wlan_inject_iface = kwargs.get('wlan_inject_iface')
+        sta_ip_addr = kwargs.get('sta_ip_addr')
+        servingAP_ip_addr = kwargs.get('servingAP_ip_addr')
+        servingAP_channel = kwargs.get('servingAP_channel')
+        servingAP_bssid = kwargs.get('servingAP_bssid')
+        targetAP_ip_addr = kwargs.get('targetAP_ip_addr')
+        targetAP_channel = kwargs.get('targetAP_channel')
+        gateway = kwargs.get('gateway')
+        ho_type = kwargs.get('ho_type')
+
+        self.log.debug("Handover of station: {} from {} to {}".format(sta_mac_addr, servingAP_ip_addr, targetAP_ip_addr))
 
         #if not (isinstance(servingAP, Node) or isinstance(targetAP, Node)):
         #    raise exceptions.InvalidArgumentException(func_name='nf.perform_STA_handover')
 
-        if hoType == HandoverTypes.Soft_CSA:
+        if ho_type == HandoverTypes.Soft_CSA:
             # register STA in target AP
             UPIargs = {'iface' : wifi_intf, 'sta_mac_addr' : sta_mac_addr}
 
             # exec on each node
-            res = self.controller.nodes(targetAP).iface(wifi_intf).blocking(True).net.register_new_STA_in_AP(UPIargs)
+            res = self.controller.nodes(targetAP).iface(wifi_intf).blocking(True).net.register_new_device(UPIargs)
             self.log.debug('registerNewSTAInAP::result: %s', str(res))
         else:
             pass
@@ -78,63 +64,56 @@ class HandoverModule(wishful_controller.ControllerUpiModule):
         res = self.controller.nodes(gateway).blocking(True).net.change_routing(UPIargs)
         self.log.info('changeRouting::result: %s', str(res))
 
-        if hoType == HandoverTypes.Soft_CSA:
+        if ho_type == HandoverTypes.Soft_CSA:
 
             # send CSA packet to client STA
             self.log.warn('Send CSA by serving AP %s to STA %s to switch channel to %s.' % (
                 servingAP, sta_mac_addr, str(targetAP_channel)))
 
-            UPIargs = {'iface' : wlan_inject_iface, 'bssid' : servingAP_bssid, 'sta_mac_addr' : sta_mac_addr,
-                       'target_channel' : targetAP_channel, 'serving_channel' : servingAP_channel}
-
             # exec on servingAP node
-            res = self.controller.nodes(servingAP).iface(wifi_intf).blocking(True).net.send_CSA_beacon_to_STA(UPIargs)
+            res = self.controller.nodes(servingAP).iface(wifi_intf).blocking(True).net.trigger_channel_switch_in_device(
+                wlan_inject_iface, sta_mac_addr, targetAP_channel, servingAP_channel, bssid=servingAP_bssid)
             self.log.info('sendCSABeaconToSTA::result: %s', str(res))
 
         else:
             # unblacklist STA on target AP
             self.log.debug('UN-Blacklist STA %s on target  AP %s.' % (sta_mac_addr, targetAP))
 
-            UPIargs = {'iface' : wifi_intf, 'sta_mac_addr' : sta_mac_addr}
-
             # exec on targetAP
-            res = self.controller.nodes(targetAP).iface(wifi_intf).blocking(True).net.remove_STA_from_AP_blacklist(UPIargs)
+            res = self.controller.nodes(targetAP).iface(wifi_intf).blocking(True).net.remove_device_from_blacklist(wifi_intf, sta_mac_addr)
             self.log.info('removeSTAFromAPBlacklist::result: %s', str(res))
 
             # blacklist STA on serving AP
             self.log.info('Blacklist STA %s on serving  AP %s ' % (sta_mac_addr, servingAP))
 
-            UPIargs = {'iface' : wifi_intf, 'sta_mac_addr' : sta_mac_addr}
-
             # exec on targetAP
-            res = self.controller.nodes(servingAP).iface(wifi_intf).blocking(True).net.add_STA_to_AP_blacklist(UPIargs)
+            res = self.controller.nodes(servingAP).iface(wifi_intf).blocking(True).net.add_device_to_blacklist(wifi_intf, sta_mac_addr)
             self.log.info('addSTAtoAPBlacklist::result: %s', str(res))
 
             # disassociate with current AP
             self.log.info('Send Disassociation Frame by serving AP %s to STA %s.' % (servingAP, sta_mac_addr))
 
-            UPIargs = {'iface' : wifi_intf, 'sta_mac_addr' : sta_mac_addr}
             # exec on serving
-            res = self.controller.nodes(servingAP).iface(wifi_intf).blocking(True).net.transmit_disassociation_request_to_STA(UPIargs)
+            res = self.controller.nodes(servingAP).iface(wifi_intf).blocking(True).net.disconnect_device(sta_mac_addr)
             self.log.info('transmitDisassociationRequestToSTA::result: %s', str(res))
 
         return True
 
 
     @wishful_controller.bind_function(upis.global_upi.get_servingAP)
-    def get_servingAP(self, nodes, wifi_intf, sta_mac_addr):
+    def is_associated_with(self, nodes, wifi_intf, sta_mac_addr):
         """
         Estimates the AP which serves the given STA. Note: if an STA is associated with multiple APs the one with the
         smallest inactivity time is returned.
         """
 
-        self.log.debug('Function: get_servingAP')
+        self.log.debug('Function: is_associated_with')
 
         try:
             nodes_with_sta = {}
 
             for node in nodes:
-                res = self.controller.nodes(node).iface(wifi_intf).blocking(True).radio.get_inactivity_time_of_associated_STAs()
+                res = self.controller.nodes(node).iface(wifi_intf).blocking(True).radio.get_inactivity_time_of_connected_devices()
 
                 if sta_mac_addr in res:
                     self.log.debug(res[sta_mac_addr])
