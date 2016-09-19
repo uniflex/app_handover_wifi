90a62d77bbb350bf7027fa3d95d4b71a4bde0998
Tolja Zubow <zubow@tkn.tu-berlin.de>
2016-09-19 13:54:08 +0200

    Two new apps: handover & wireless stats

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ setup(
     packages=find_packages(),
     url='http://www.wishful-project.eu/software',
     license='',
-    author='Piotr Gawlowicz',
-    author_email='gawlowicz@tu-berlin.de',
-    description='WiSHFUL Handover Module',
-    long_description='Implementation of a wireless agent using the unified programming interfaces (UPIs) of the Wishful project.',
+    author='Anatolij Zubow, Piotr Gawlowicz',
+    author_email='zubow|gawlowicz@tu-berlin.de',
+    description='WiFi (802.11) Handover Module',
+    long_description='Implementation of the handover app for WiFi.',
     keywords='wireless control',
     install_requires=[]
 )
--- wishful_module_handover/handover_module.py
+++ wishful_module_handover/handover_module.py
@@ -3,78 +3,112 @@ import wishful_framework as wishful_module
 import wishful_upis as upis
 from wishful_framework.classes import exceptions
 
-__author__ = "Piotr Gawlowicz, Anatolij Zubow"
+__author__ = "Anatolij Zubow"
 __copyright__ = "Copyright (c) 2015, Technische Universitat Berlin"
 __version__ = "0.1.0"
-__email__ = "{gawlowicz, zubow}@tkn.tu-berlin.de"
+__email__ = "{zubow}@tkn.tu-berlin.de"
 
-class HandoverTypes:
-    Hard_BL, Soft_CSA = list(range(2))
 
 '''
-    This is IEEE 802.11 specific -> todo: rename it into handover_wifi
+    Handover module for IEEE 802.11 with two submodules: i) soft and ii) hard HO.
 '''
 @wishful_module.build_module
-class HandoverModule(wishful_module.ControllerModule):
+class WiFiHandoverModule(wishful_module.ControllerModule):
     def __init__(self, controller):
-        super(HandoverModule, self).__init__(controller)
+        super(WiFiHandoverModule, self).__init__()
         self.log = logging.getLogger('wifi_handover_module.main')
+        self.nodes = {}
 
-    @wishful_module.bind_function(upis.net_func.perform_handover)
-    def perform_handover(self, wifi_intf, servingAP, targetAP, sta_mac_addr, **kwargs):
+    @wishful_module.on_start()
+    def start_ho_module(self):
+        self.log.debug("Start HO module".format())
+        self.running = True
+
+    @wishful_module.on_exit()
+    def stop_ho_module(self):
+        self.log.debug("Stop HO module".format())
+        self.running = False
+
+
+    @wishful_module.on_event(upis.mgmt.NewNodeEvent)
+    def add_node(self, event):
+        node = event.node
+
+        if self.mode == "GLOBAL" and node.local:
+            return
+
+        self.log.info("Added new node: {}, Local: {}"
+                      .format(node.uuid, node.local))
+        self.nodes[node.uuid] = node
+
+
+    @wishful_module.on_event(upis.mgmt.NodeExitEvent)
+    @wishful_module.on_event(upis.mgmt.NodeLostEvent)
+    def remove_node(self, event):
+        self.log.info("Node lost".format())
+        node = event.node
+        reason = event.reason
+        if node.uuid in self.nodes:
+            del self.nodes[node.uuid]
+            self.log.info("Node: {}, Local: {} removed reason: {}"
+                          .format(node.uuid, node.local, reason))
+
+
+    @wishful_module.on_event(upis.wifi.WiFiTriggerHandoverRequestEvent)
+    def perform_handover(self, event):
 
         """
         Performing an infrastructure-initiated handover of a client STA using either:
         - ho_type = 0: hard HO scheme,
         - ho_type = 1: soft HO scheme
         """
+        is_soft_ho = False
+        if event.ho_scheme == 'Soft_CSA':
+            is_soft_ho = True
 
-        wlan_inject_iface = kwargs.get('wlan_inject_iface')
-        sta_ip_addr = kwargs.get('sta_ip_addr')
-        servingAP_ip_addr = kwargs.get('servingAP_ip_addr')
-        servingAP_channel = kwargs.get('servingAP_channel')
-        servingAP_bssid = kwargs.get('servingAP_bssid')
-        targetAP_ip_addr = kwargs.get('targetAP_ip_addr')
-        targetAP_channel = kwargs.get('targetAP_channel')
-        gateway = kwargs.get('gateway')
-        ho_type = kwargs.get('ho_type')
+        # from UUID to node object
+        servingAP = self.nodes[event.serving_node]
+        targetAP = self.nodes[event.targetAP_node]
+        gateway = self.nodes[event.gateway]
+
+        sta_mac_addr = event.sta_id
+        wlan_inject_iface = event.wlan_inject_iface
+        sta_ip_addr = event.sta_ip
+
+        servingAP_ip_addr = event.servingAP_ip
+        servingAP_channel = event.servingChannel
+        servingAP_bssid = event.network_bssid
+        targetAP_ip_addr = event.targetAP_ip
+        targetAP_channel = event.targetChannel
+        wifi_intf = event.wlan_iface
 
         self.log.debug("Handover of station: {} from {} to {}".format(sta_mac_addr, servingAP_ip_addr, targetAP_ip_addr))
 
         #if not (isinstance(servingAP, Node) or isinstance(targetAP, Node)):
         #    raise exceptions.InvalidArgumentException(func_name='nf.perform_STA_handover')
 
-        if ho_type == HandoverTypes.Soft_CSA:
+        if is_soft_ho:
             # register STA in target AP
-            UPIargs = {'iface' : wifi_intf, 'sta_mac_addr' : sta_mac_addr}
-
-            # exec on each node
-            res = self.controller.nodes(targetAP).iface(wifi_intf).blocking(True).net.register_new_device(UPIargs)
+            res = targetAP.iface(wifi_intf).blocking(True).net.register_new_device(wifi_intf, sta_mac_addr)
             self.log.debug('registerNewSTAInAP::result: %s', str(res))
         else:
             pass
 
         # Set ARP entry for STA on target AP
-        UPIargs = {'iface' : wifi_intf, 'mac_addr' : sta_mac_addr, 'ip_addr' : sta_ip_addr}
-
-        res = self.controller.nodes(targetAP).blocking(True).net.set_ARP_entry(UPIargs)
+        res = targetAP.blocking(True).net.set_ARP_entry(wifi_intf, sta_mac_addr, sta_ip_addr)
         self.log.info('setARPEntry::result: %s', str(res))
 
         # change routing in backhaul by adapting routing on gateway
-        UPIargs = {'servingAP_ip_addr' : servingAP_ip_addr, 'targetAP_ip_addr' : targetAP_ip_addr, 'sta_ip_addr' : sta_ip_addr}
-
-        # exec on gateway node
-        res = self.controller.nodes(gateway).blocking(True).net.change_routing(UPIargs)
+        res = gateway.blocking(True).net.change_routing(servingAP_ip_addr, targetAP_ip_addr, sta_ip_addr)
         self.log.info('changeRouting::result: %s', str(res))
 
-        if ho_type == HandoverTypes.Soft_CSA:
-
+        if is_soft_ho:
             # send CSA packet to client STA
             self.log.warn('Send CSA by serving AP %s to STA %s to switch channel to %s.' % (
                 servingAP, sta_mac_addr, str(targetAP_channel)))
 
             # exec on servingAP node
-            res = self.controller.nodes(servingAP).iface(wifi_intf).blocking(True).net.trigger_channel_switch_in_device(
+            res = servingAP.iface(wifi_intf).blocking(True).net.trigger_channel_switch_in_device(
                 wlan_inject_iface, sta_mac_addr, targetAP_channel, servingAP_channel, bssid=servingAP_bssid)
             self.log.info('sendCSABeaconToSTA::result: %s', str(res))
 
@@ -83,59 +117,23 @@ class HandoverModule(wishful_module.ControllerModule):
             self.log.debug('UN-Blacklist STA %s on target  AP %s.' % (sta_mac_addr, targetAP))
 
             # exec on targetAP
-            res = self.controller.nodes(targetAP).iface(wifi_intf).blocking(True).net.remove_device_from_blacklist(wifi_intf, sta_mac_addr)
+            res = targetAP.iface(wifi_intf).blocking(True).net.remove_device_from_blacklist(wifi_intf, sta_mac_addr)
             self.log.info('removeSTAFromAPBlacklist::result: %s', str(res))
 
             # blacklist STA on serving AP
             self.log.info('Blacklist STA %s on serving  AP %s ' % (sta_mac_addr, servingAP))
 
             # exec on targetAP
-            res = self.controller.nodes(servingAP).iface(wifi_intf).blocking(True).net.add_device_to_blacklist(wifi_intf, sta_mac_addr)
+            res = servingAP.iface(wifi_intf).blocking(True).net.add_device_to_blacklist(wifi_intf, sta_mac_addr)
             self.log.info('addSTAtoAPBlacklist::result: %s', str(res))
 
             # disassociate with current AP
             self.log.info('Send Disassociation Frame by serving AP %s to STA %s.' % (servingAP, sta_mac_addr))
 
             # exec on serving
-            res = self.controller.nodes(servingAP).iface(wifi_intf).blocking(True).net.disconnect_device(sta_mac_addr)
+            res = servingAP.iface(wifi_intf).blocking(True).net.disconnect_device(sta_mac_addr)
             self.log.info('transmitDisassociationRequestToSTA::result: %s', str(res))
 
-        return True
-
-
-    @wishful_module.bind_function(upis.net_func.get_servingAP)
-    def is_associated_with(self, nodes, wifi_intf, sta_mac_addr):
-        """
-        Estimates the AP which serves the given STA. Note: if an STA is associated with multiple APs the one with the
-        smallest inactivity time is returned.
-        """
-
-        self.log.debug('Function: is_associated_with')
-
-        try:
-            nodes_with_sta = {}
-
-            for node in nodes:
-                res = self.controller.nodes(node).iface(wifi_intf).blocking(True).radio.get_inactivity_time_of_connected_devices()
-
-                if sta_mac_addr in res:
-                    self.log.debug(res[sta_mac_addr])
-                    nodes_with_sta[node] = int(res[sta_mac_addr][0])
-
-                    # dictionary of aps where station is associated
-                    self.log.debug("STA found on the following APs with the following idle times:")
-                    self.log.debug(str(nodes_with_sta))
-
-            if not bool(nodes_with_sta):
-                # If no serving AP was found; return None
-                return None
-
-            # serving AP is the one with minimal STA idle value
-            servingAP = min(nodes_with_sta, key=nodes_with_sta.get)
-            self.log.info("STA %s is served by AP %s " % (sta_mac_addr, servingAP))
-
-            return servingAP
-
-        except Exception as e:
-            self.log.fatal("An error occurred in get_servingAP: %s" % e)
-            raise e
+        # send reply event
+        reply_event = upis.net_func.TriggerHandoverReplyEvent(True)
+        self.send_event(reply_event)
